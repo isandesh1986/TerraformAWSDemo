f0810017a0c3370b0ed719bd7bb368ea333c58c8
Sandesh, Pallapati Immanuel Prabhu <immanuel-prabhu.sandesh-pallapati@capgemini.com>
2022-05-09 15:36:40 +0530

    Create main.tf

---
+++ tf-s3/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region     = var.region  
+}
+
+resource "random_integer" "rand_int" {
+  min = 10000
+  max = 99999  
+}
+
+resource "aws_s3_bucket" "aws-b1" {
+  bucket = "ipsbucket-${random_integer.rand_int.result}"
+  acl    = "private"
+}
